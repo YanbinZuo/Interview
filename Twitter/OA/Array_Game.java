50b69d1c4f15ef0ff1db1902d61a381a64c714e7
shangde <shangdehan@gmail.com>
2021-09-24 20:58:31 -0700

    Update Array_Game.java

--- Twitter/OA/Array_Game.java
+++ Twitter/OA/Array_Game.java
@@ -3,26 +3,23 @@
 class minOperation{
    /* Function to print minimum operation required
        to make all elements of an array equal */
-   static void printMinOp(int arr[]){
+   public static long printMinOp(int arr[]){
+      int min = Integer.MAX_VALUE;
       int sum = 0;
-      int min = arr[0];
-      int op = 0;
-      int n = arr.length;
-      for(int i=0; i<n; i++){
+      int res = 0;
+      for(int i=0; i<arr.length; i++){
          sum += arr[i];
-         if(arr[i] < min){
-            min = arr[i];
-         }
+         min = Math.min(min, arr[i]);
       }
-      op = sum - n*min;
-      System.out.println("Minimum operation = " + op);
+      res = sum - arr.length*min;
+      return res;
    }
    
    public static void main(String[] args){
       int arr[] = {5,6,2,4,3};
-      printMinOp(arr);
+      long res1 = printMinOp(arr);
       int arr1[] = {3,4,6,6,3};
-      printMinOp(arr1);
+      long res = printMinOp(arr1);
+      System.out.println(res1);
    }
-
 } 

2bfd1ae33b4504ed46f2bb8cf85bab726b0f6c1f
shangde <shangdehan@gmail.com>
2021-10-03 19:01:01 -0700

    Update Counting analogous Array.java

--- Tiktok/OA/Counting analogous Array.java
+++ Tiktok/OA/Counting analogous Array.java
@@ -11,3 +11,25 @@ public static int countAnalogousArrays(List<Integer> consecutiveDifference, int 
    return Math.max(0, (upperBound-lowerBound) - (max-min)+1)
 
 }
+
+
+public static int countAnalogousArrays(List<Integer> consecutiveDifference, int lowerBound, int upperBound) {
+ 
+   int count = 0;
+   long max = lowerBound;
+   long min = lowerBound;
+   long prev= lowerBound;
+   long curr = 0;
+   for(int i = 1; i <= consectutiveDifference.size(); i++){
+       curr = prev - consecuiveDifference.get(i-1);
+        min = Math.min(min, curr);
+        max = Math.max(max, curr);
+        prev = cur;
+   }
+   while(max <= upperBound){
+       if(min >= lowerBound)count++;
+       min = min+1l
+       max = max+1;
+   }
+   return count;
+}
